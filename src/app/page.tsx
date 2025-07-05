615ebbbaaa1422a7692d2e75fcb7f70ecd72edcf
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-07-06 01:55:07 +0530

    Add more things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
 
+
 import TopBar from "./components/TopBar/TopBar";
 
 export default function Home() {
   return (
     <div>
         <TopBar/>
+    
+        <div style={{ height: "5000px" }}>
+        </div>
     </div>
     
   );

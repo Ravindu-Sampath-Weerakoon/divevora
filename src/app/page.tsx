cefd9123d960bdd8c4884e72096f72095e6f21d4
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-07-07 22:24:02 +0530

    themes update
    themes update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
 
-
 import NavBar from "./components/NavBar/NavBar";
 import TopBar from "./components/TopBar/TopBar";
+
 
 export default function Home() {
   return (

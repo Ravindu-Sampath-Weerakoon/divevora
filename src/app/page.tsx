0d4a6345a1d3e0c4531f9636bbaff1ea1d32803b
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-07-07 22:00:13 +0530

    Update some thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@
 // import styles from "./page.module.css";
 
 
+import NavBar from "./components/NavBar/NavBar";
 import TopBar from "./components/TopBar/TopBar";
 
 export default function Home() {
   return (
     <div>
         <TopBar/>
-    
+        <NavBar/>
         <div style={{ height: "5000px" }}>
         </div>
     </div>
